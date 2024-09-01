5923943a333b1f05564d9ea11865df35484c07bc
Maxim Davidson <maxd02122004@gmail.com>
2024-09-01 20:50:05 +0300

    feat: Ended the maket

--- src/app/categoryPage/components/CategorySearch/index.tsx
+++ src/app/categoryPage/components/CategorySearch/index.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 import style from './style.module.scss';
 
 export const CategorySearch = () => {
@@ -11,6 +12,60 @@ export const CategorySearch = () => {
           className={style.searchInput}
         />
         <button className={style.searchButton}>Search</button>
+      </div>
+      <div className={style.item_container}>
+        <h1 className={style.item_container_title}>Categories</h1>
+        <div className={style.search_item}>
+          <Image
+            src="/images/shuttle.png"
+            alt="Shuttle Image"
+            width={48}
+            height={48}
+            className={style.ico}
+          />
+          <h2 className={style.item_title}>Startup</h2>
+        </div>
+        <div className={style.search_item}>
+          <Image
+            src="/images/business.png"
+            alt="Business Image"
+            width={48}
+            height={48}
+            className={style.ico}
+          />
+          <h2 className={style.item_title}>Startup</h2>
+        </div>
+        <div className={style.search_item}>
+          <Image
+            src="/images/economy.png"
+            alt="Economy Image"
+            width={48}
+            height={48}
+            className={style.ico}
+          />
+          <h2 className={style.item_title}>Startup</h2>
+        </div>
+        <div className={style.search_item}>
+          <Image
+            src="/images/technology.png"
+            alt="Technology Image"
+            width={48}
+            height={48}
+            className={style.ico}
+          />
+          <h2 className={style.item_title}>Startup</h2>
+        </div>
+      </div>
+      <div className={style.tags}>
+        <h1 className={style.tags_item}>All Tags</h1>
+        <div className={style.tags_list}>
+          <span className={style.tag}>Business</span>
+          <span className={style.tag}>Experience</span>
+          <span className={style.tag}>Screen</span>
+          <span className={style.tag}>Technology</span>
+          <span className={style.tag}>Marketing</span>
+          <span className={style.tag}>Life</span>
+        </div>
       </div>
     </div>
   );
