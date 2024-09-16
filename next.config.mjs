42ba0c2024b778a6356474a86f04591a95d2542d
Maxim Davidson <maxd02122004@gmail.com>
2024-09-16 14:47:13 +0300

    next config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
--- src/app/[locale]/privatePolicyPage/page.tsx
+++ src/app/[locale]/privatePolicyPage/page.tsx
@@ -1,6 +1,4 @@
 import { ErrorBoundary } from '@/components/ErrorBoundary';
-import { Footer } from '@/components/Footer';
-import { Header } from '@/components/Header';
 import { PolicyInfo } from './components/PolyceInfo';
 
 const privatePolicyPage = () => {
