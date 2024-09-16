a74040fed6c95eda252c50cd4fe68b45fb5ecedd
Maxim Davidson <maxd02122004@gmail.com>
2024-09-16 10:28:00 +0300

    fix: Some changes in e2e test

--- cypress/e2e/categoryLink.cy.ts
+++ cypress/e2e/categoryLink.cy.ts
@@ -8,11 +8,11 @@ describe('Category Navigation', () => {
     cy.visit(appHost);
   });
 
-  categories.forEach(({ title }) => {
-    it(`should navigate to the ${title} category page when clicking on the ${title} card`, () => {
-      cy.contains('a', title, { timeout: 5000 }).should('exist').click();
+  categories.forEach(({ key }) => {
+    it(`should navigate to the ${key} category page when clicking on the ${key} card`, () => {
+      cy.contains('a', key, { timeout: 5000 }).should('exist').click();
       cy.wait(1000);
-      cy.url().should('include', `${Routes.Category}/${title}`);
+      cy.url().should('include', `${Routes.Category}/${key}`);
     });
   });
 });
