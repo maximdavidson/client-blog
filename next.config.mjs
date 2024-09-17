451b2f896b5ee958ae80b238024707844b3812a7
Maxim Davidson <maxd02122004@gmail.com>
2024-09-17 12:14:21 +0300

    Change alias

--- next.config.mjs
+++ next.config.mjs
@@ -12,13 +12,14 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@/': path.resolve(__dirname, 'src'),
       '@/constants': path.resolve(__dirname, 'src/constants'),
       '@/components': path.resolve(__dirname, 'src/components'),
       '@/app': path.resolve(__dirname, 'src/app'),
       '@/data': path.resolve(__dirname, 'src/data'),
       '@/styles': path.resolve(__dirname, 'src/styles'),
       '@/UI': path.resolve(__dirname, 'src/UI'),
+      '@/navigation': path.resolve(__dirname, 'src/navigation'),
+      '@/utils': path.resolve(__dirname, 'src/utils'),
     };
     return config;
   },
